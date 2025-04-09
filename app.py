36ca23d03bee81b8d4cd4a4aec5cb3ec0f7a293b
Lukas <21Nuga@pm.me>
2025-04-09 18:09:56 +0200

    feat: added whole CRUD
    - implemented delete route and feature
    - implemented update route and feature
    
    - improved style

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-from functions import load_json, save_json, get_new_id
+from functions import load_json, save_json, get_new_id, fetch_post_by_id, add_blog_post, update_blog_post
 
 app = Flask(__name__)
 
@@ -15,23 +15,49 @@ def index():
 def add():
     if request.method == 'POST':
         blog_posts = load_json("blog_posts.json")
+
         new_id = get_new_id(blog_posts)
         author = request.form.get("author")
         title = request.form.get("title")
         content = request.form.get("content")
-        print("DEBUG", new_id, author, title, content)
-        blog_posts.append(
-            {
-                "id": new_id,
-                "author": author,
-                "title": title,
-                "content": content
-            }
-        )
-        save_json(blog_posts, "blog_posts.json")
+
+        add_blog_post(new_id, author, title, content, blog_posts)
+
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/delete/<post_id>')
+def delete(post_id):
+    blog_posts = load_json("blog_posts.json")
+
+    post = fetch_post_by_id(post_id, blog_posts)
+
+    blog_posts.remove(post)
+
+    save_json(blog_posts, "blog_posts.json")
+
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    posts = load_json("blog_posts.json")
+    post = fetch_post_by_id(post_id, posts)
+
+    if not post:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        update_blog_post(post_id, author, title, content, posts)
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
